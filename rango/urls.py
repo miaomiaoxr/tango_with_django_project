fc435c7f1d796c38073cc232f18c9896f7edb333
miaomiaoxr <miaomiaoxr1234@gmail.com>
2022-01-27 16:14:15 +0000

    Chapter 3 finished

---
+++ rango/urls.py
@@ -0,0 +1,10 @@
+from os import abort
+from django.urls import URLPattern, path
+from rango import views
+
+app_name = 'rango'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about')
+]
