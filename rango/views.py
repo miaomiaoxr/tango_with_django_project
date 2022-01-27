33cafc4ee90eac1a85471f525bcd202a137db708
miaomiaoxr <miaomiaoxr1234@gmail.com>
2022-01-27 19:52:01 +0000

    Chapter 4 Finished

--- rango/views.py
+++ rango/views.py
@@ -4,9 +4,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    html = "Rango says hey there partner! " + "<a href='/rango/about/'>About</a>"
-    return HttpResponse(html)
+    # html = "Rango says hey there partner! " + "<a href='/rango/about/'>About</a>"
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request,'rango/index.html',context=context_dict)
 
 def about(request):
-    html ="Rango says here is the about page. " + "<a href='/rango/'>Index</a>"
-    return HttpResponse(html)
+    context_dict = {'boldmessage': 'This tutorial has been put together by MIAO'}
+    return render(request,'rango/about.html',context=context_dict)
