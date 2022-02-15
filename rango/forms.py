6f7ffc83bc30b052869d4997ed93ba78268b636d
Shoumiao Liu <2711014L@student.gla.ac.uk>
2022-02-15 18:18:32 +0000

    Chapter 9 finished

--- rango/forms.py
+++ rango/forms.py
@@ -1,7 +1,8 @@
 from dataclasses import field
 from turtle import title
 from django import forms
-from rango.models import Page,Category
+from rango.models import Page,Category,UserProfile
+from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.NAME_MAX_LENGTH,help_text="Please enter the category name.")
@@ -22,3 +23,14 @@ class PageForm(forms.ModelForm):
         model = Page
         exclude = ('category',)
     
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username','email','password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website','picture',)
