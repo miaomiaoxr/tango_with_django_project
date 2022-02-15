231296b14480bbb1034fa42f231edb191be0a15a
Shoumiao Liu <2711014L@student.gla.ac.uk>
2022-02-15 22:49:06 +0000

    Chapter 10 finished

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unicodedata import category
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
@@ -8,6 +9,27 @@ from rango.models import Category
 from rango.models import Page
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+
+def get_server_side_cookie(request,cookie,default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request,'visits','1'))
+
+    last_visit_cookie = get_server_side_cookie(request,'last_visit',str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    
+
+    if (datetime.now()-last_visit_time).days>0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    
+    request.session['visits'] = visits
 
 @login_required
 def user_logout(request):
@@ -133,8 +155,22 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    return render(request,'rango/index.html',context=context_dict)
+
+    visitor_cookie_handler(request)
+
+    response=render(request,'rango/index.html',context=context_dict)
+    return response
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by MIAO'}
-    return render(request,'rango/about.html',context=context_dict)
+    context_dict = {}
+    context_dict['boldmessage'] = 'This tutorial has been put together by MIAO'
+
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request,'rango/about.html',context=context_dict)
+    return response
